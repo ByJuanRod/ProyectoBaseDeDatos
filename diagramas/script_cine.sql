dd757501dd10f47ae38e7cd65b0be629f40c043e
conil <atbc0002@ce.pucmm.edu.do>
2026-07-17 14:12:46 -0400

    Script mejorado

--- diagramas/script_cine.sql
+++ diagramas/script_cine.sql
@@ -1,93 +1,157 @@
-create table Clientes(
-	codigo int auto_increment primary key,
-	nombres varchar(50) not null,
-	apellidos varchar(50) not null,
-	telefono varchar(15) not null,
-	fecha_registro date default current_date not null,
-	cantidad_entradas int not null
+CREATE TABLE Ciudades (
+  codigo INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(100) NOT NULL,
+  codigo_postal VARCHAR(20)
 );
 
-create table Generos(
-    codigo int auto_increment primary key,
-    nombre varchar(30)
+CREATE TABLE Generos (
+    codigo INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL
 );
-
 INSERT INTO generos (nombre) VALUES
- ('Acción'),
- ('Aventura'),
- ('Animación'),
- ('Comedia'),
- ('Ciencia Ficción'),
- ('Documental'),
- ('Drama'),
- ('Fantasía'),
- ('Musical'),
- ('Misterio'),
- ('Romance'),
- ('Suspenso'),
- ('Terror'),
- ('Western'),
- ('Bélico'),
- ('Deportes'),
- ('Crimen'),
- ('Histórico'),
- ('Familiar'),
- ('Artes Marciales');
+     ('Acción'),
+     ('Aventura'),
+     ('Animación'),
+     ('Comedia'),
+     ('Ciencia Ficción'),
+     ('Documental'),
+     ('Drama'),
+     ('Fantasía'),
+     ('Musical'),
+     ('Misterio'),
+     ('Romance'),
+     ('Suspenso'),
+     ('Terror'),
+     ('Western'),
+     ('Bélico'),
+     ('Deportes'),
+     ('Crimen'),
+     ('Histórico'),
+     ('Familiar'),
+     ('Artes Marciales');
 
-create table Peliculas(
-  codigo int auto_increment primary key,
-  nombre varchar(70) not null,
-  director varchar(50) not null,
-  duracion_minutos int not null,
-  clasificacion varchar(5) not null
+CREATE TABLE Clientes (
+  codigo INT AUTO_INCREMENT PRIMARY KEY,
+  nombres VARCHAR(50) NOT NULL,
+  apellidos VARCHAR(50) NOT NULL,
+  telefono VARCHAR(15),
+  fecha_nacimiento DATE,
+  cantidad_entradas INT DEFAULT 0,
+  sexo CHAR(1) CHECK(sexo = 'M' or sexo = 'F') NOT NULL,
+  correo VARCHAR(50) UNIQUE NOT NULL
 );
 
-create table Salas(
-	codigo int auto_increment primary key,
-	nombre varchar(10) not null,
-	capacidad int not null
+CREATE TABLE Peliculas (
+   codigo INT AUTO_INCREMENT PRIMARY KEY,
+   nombre VARCHAR(100) NOT NULL,
+   codigo_director INT NOT NULL,
+   FOREIGN KEY (codigo_director) REFERENCES Directores(codigo)
+   duracion_minutos INT,
+   clasificacion VARCHAR(10),
+   sinopsis VARCHAR(400),
+   fecha_nacimiento DATE,
+   portada BLOB
 );
 
-create table Funciones(
-	codigo int auto_increment primary key,
-	codigo_pelicula int not null,
-	foreign key (codigo_pelicula) references Peliculas (codigo),
-	codigo_sala int not null,
-	foreign key (codigo_sala) references Salas (codigo),
-	fecha date default current_date not null,
-	hora_inicio time not null,
-	hora_fin time not null,
-	precio_entrada decimal(10,2) not null
+CREATE TABLE Sucursales (
+    codigo INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    direccion VARCHAR(200),
+    telefono VARCHAR(15),
+    correo VARCHAR(50) UNIQUE NOT NULL,
+    codigo_ciudad INT NOT NULL,
+    FOREIGN KEY (codigo_ciudad) REFERENCES Ciudades(codigo)
 );
 
-create table Asientos(
-	codigo int auto_increment primary key,
-	codigo_sala int not null,
-	foreign key (codigo_sala) references Salas (codigo) on delete cascade,
-	tipo varchar(20)
+CREATE TABLE Generos_Peliculas (
+   codigo_pelicula INT NOT NULL,
+   codigo_generos INT NOT NULL,
+   PRIMARY KEY (codigo_pelicula, codigo_generos),
+   FOREIGN KEY (codigo_pelicula) REFERENCES Peliculas(codigo) ON DELETE CASCADE,
+   FOREIGN KEY (codigo_generos) REFERENCES Generos(codigo) ON DELETE CASCADE
 );
 
-create table Ventas(
-	codigo int auto_increment primary key,
-	precio_total decimal(10,2) not null,
-	fecha date default current_date not null,
-	hora time not null,
-	codigo_cliente int not null,
-	foreign key (codigo_cliente) references Clientes (codigo)
-);
-create table Boletos(
-	codigo int auto_increment primary key,
-	codigo_factura int not null,
-	foreign key (codigo_factura) references Ventas (codigo),
-	codigo_funcion int not null,
-	foreign key (codigo_funcion) references Funciones (codigo),
-	codigo_asiento int not null,
-	foreign key (codigo_asiento) references Asientos (codigo_asiento)
+CREATE TABLE Salas (
+   codigo INT AUTO_INCREMENT PRIMARY KEY,
+   nombre VARCHAR(50) NOT NULL,
+   capacidad INT NOT NULL
 );
 
-create table Generos_Peliculas(
-    codigo_pelicula int not null,
-    foreign key (codigo_pelicula) references Peliculas(codigo),
-    codigo_generos int not null,
-    foreign key (codigo_generos) references Generos(codigo)
+CREATE TABLE Empleados (
+   codigo INT AUTO_INCREMENT PRIMARY KEY,
+   nombres VARCHAR(50) NOT NULL,
+   apellidos VARCHAR(50) NOT NULL,
+   telefono VARCHAR(15),
+   correo VARCHAR(50) UNIQUE NOT NULL,
+   puesto VARCHAR(20) NOT NULL,
+   codigo_sucursal INT NOT NULL,
+   FOREIGN KEY (codigo_sucursal) REFERENCES Sucursales(codigo)
+);
+
+CREATE TABLE Asientos (
+    codigo INT AUTO_INCREMENT PRIMARY KEY,
+    numero INT NOT NULL,
+    fila VARCHAR(5) NOT NULL,
+    codigo_sala INT NOT NULL,
+    FOREIGN KEY (codigo_sala) REFERENCES Salas(codigo)
+);
+
+CREATE TABLE Funciones (
+   codigo INT AUTO_INCREMENT PRIMARY KEY,
+   fecha DATE NOT NULL,
+   hora_inicio TIME NOT NULL,
+   hora_fin TIME NOT NULL,
+   precio_entrada DECIMAL(10,2) NOT NULL,
+   codigo_pelicula INT NOT NULL,
+   codigo_sala INT NOT NULL,
+   FOREIGN KEY (codigo_pelicula) REFERENCES Peliculas(codigo),
+   FOREIGN KEY (codigo_sala) REFERENCES Salas(codigo)
+);
+
+CREATE TABLE Ventas (
+    codigo INT AUTO_INCREMENT PRIMARY KEY,
+    fecha DATE NOT NULL,
+    hora TIME NOT NULL,
+    precio_total DECIMAL(10,2) NOT NULL,
+    codigo_cliente INT NOT NULL,
+    codigo_empleado INT NOT NULL,
+    codigo_sucursal INT NOT NULL,
+    FOREIGN KEY (codigo_cliente) REFERENCES Clientes(codigo),
+    FOREIGN KEY (codigo_empleado) REFERENCES Empleados(codigo),
+    FOREIGN KEY (codigo_sucursal) REFERENCES Sucursales(codigo)
+);
+
+CREATE TABLE Boletos (
+ codigo INT AUTO_INCREMENT PRIMARY KEY,
+ precio_aplicado DECIMAL(10,2) NOT NULL,
+ codigo_venta INT NOT NULL,
+ codigo_funcion INT NOT NULL,
+ codigo_asiento INT NOT NULL,
+ FOREIGN KEY (codigo_venta) REFERENCES Ventas(codigo) ON DELETE CASCADE,
+ FOREIGN KEY (codigo_funcion) REFERENCES Funciones(codigo),
+ FOREIGN KEY (codigo_asiento) REFERENCES Asientos(codigo)
+);
+
+CREATE TABLE Actores(
+   codigo INT AUTO_INCREMENT PRIMARY KEY,
+   nombres VARCHAR(50) NOT NULL,
+   apellidos VARCHAR(50) NOT NULL,
+   fecha_nacimiento DATE,
+   sexo CHAR(1) CHECK(sexo = 'M' or sexo = 'F') NOT NULL
+);
+
+CREATE TABLE Directores(
+    codigo INT AUTO_INCREMENT PRIMARY KEY,
+    nombres VARCHAR(50) NOT NULL,
+    apellidos VARCHAR(50) NOT NULL,
+    fecha_nacimiento DATE,
+    sexo CHAR(1) CHECK(sexo = 'M' or sexo = 'F') NOT NULL
+);
+
+CREATE TABLE Actores_Peliculas (
+   codigo_pelicula INT NOT NULL,
+   codigo_actor INT NOT NULL,
+   PRIMARY KEY (codigo_pelicula, codigo_actor),
+   FOREIGN KEY (codigo_pelicula) REFERENCES Peliculas(codigo),
+   FOREIGN KEY (codigo_actor) REFERENCES Actores(codigo)
 );
